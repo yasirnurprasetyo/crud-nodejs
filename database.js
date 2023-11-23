5126dcab7f9fbc84d73adaa919060b62171b56c2
yasirnurprasetyo <yasirnurprasetyo@gmail.com>
2023-11-23 10:05:08 +0700

    add env and fixing database connection

--- database.js
+++ database.js
@@ -1,10 +1,15 @@
 import mysql from "mysql2"
+import { config } from "dotenv"
 
-const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "crud"
-})
+const connection = () => {
+    config()
+    const {DB_HSOT, DB_NAME, DB_USER, DB_PASSWORD} = process.env
+    return mysql.createPool({
+        host: DB_HSOT,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME
+    })
+}
 
 export default connection.promise()
